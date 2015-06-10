2cb81c4fbe6edfd3d12d5d205b0895fd40b07b92
Jai Salzwedel <jai.salzwedel@cern.ch>
2015-06-10 13:34:53 -0400

    Fleshed out the shell of the main transform matrix function

--- TransformMatrices/GenerateTransformMatrix.C
+++ TransformMatrices/GenerateTransformMatrix.C
@@ -11,15 +11,42 @@
 
 
 
-void GenerateTransformMatrix(const int nFiles)
+void GenerateTransformMatrix(const Int_t nFiles)
 {
   // Main function.  Specify how many input files to use and this
   // will generate a list of file names to use and pass them on.
   
+  Int_t parent1PDG; //Pass this in somehow
+  Int_t parent2PDG;
+
   TH2D *hTransform = SetupMatrixHist();
   
   vector<TString> fileNames = GetTFileNames(nFiles);
 
+  for (Int_t iFile = 0; iFile < nFiles; iFile++)
+  {
+    TFile inFile(fileNames[iFile], "read");
+    assert(NULL != &inFile);
+
+    TTree *thermTree = (TTree*) inFile.Get("particles");
+    assert(NULL != thermTree);
+
+    // do stuff ...
+    for(/* each particular event */) {
+      vector<Int_t> lambdaIDs = GetPassingLambdaIDs(thermTree);
+      vector<Int_t> parent1IDs = GetParentIDs(&lambdaIDs, parent1PDG, thermTree);
+      vector<Int_t> parent2IDs = GetParentIDs(&lambdaIDs, parent2PDG, thermTree);
+      FillTransformMatrix(hTransform, &parent1IDs, &parent2IDs, &lambdaIDs, thermTree);
+    }
+  }
+  
+  TString outFileName = "TransformMatrices.root";
+  TFile outFile(outFileName, "update");
+  outFile.cd();
+  hTransform->Write(0,TObject::kOverwrite);
+  cout<<"Transform matrix "<<hTransform->GetName()
+      <<" written to "<<outFileName<<endl;
+  
 }
 
 
@@ -36,10 +63,12 @@ TH2D *SetupMatrixHist()
   return hTransform;
 }
 
-vector<TString> GetTFileNames(const int nFiles)
+vector<TString> GetTFileNames(const Int_t nFiles)
 {
   // Use the number of files to generate a list of event file names
   vector<TString> fileNames;
   // ...
   return fileNames;
 }
+
+
