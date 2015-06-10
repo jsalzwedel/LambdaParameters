aef305c5cc87074990c3df8b829c15571dacc5a0
Jai Salzwedel <jai.salzwedel@cern.ch>
2015-06-10 16:49:46 -0400

    Made template for GetIDsOfLambdas

--- TransformMatrices/GenerateTransformMatrix.C
+++ TransformMatrices/GenerateTransformMatrix.C
@@ -7,7 +7,9 @@
 //
 //********************************************************************
 
-
+enum ParticlePID {kProt = 2212, kAntiProt = -2212, 
+		  kPiPlus = 211, kPiMinus = -211, 
+		  kLam = 3122, kALam = -3122};
 
 
 
@@ -33,7 +35,7 @@ void GenerateTransformMatrix(const Int_t nFiles)
 
     // do stuff ...
     for(/* each particular event */) {
-      vector<Int_t> lambdaIDs = GetPassingLambdaIDs(thermTree);
+      vector<Int_t> lambdaIDs = GetIDsOfLambdas(parent1PDG, parent2PDG, thermTree);
       vector<Int_t> parent1IDs = GetParentIDs(&lambdaIDs, parent1PDG, thermTree);
       vector<Int_t> parent2IDs = GetParentIDs(&lambdaIDs, parent2PDG, thermTree);
       FillTransformMatrix(hTransform, &parent1IDs, &parent2IDs, &lambdaIDs, thermTree);
@@ -97,4 +99,59 @@ vector<TString> GetTFileNames(const Int_t nFiles)
   return fileNames;
 }
 
+vector<Int_t> GetIDsOfLambdas(const Int_t parent1PDG, const Int_T parent2PDG, const TTree *thermTree, Int_t &firstEntry, const Int_t finalEntry)
+{
+
+
+  // Get the particle branch
+  ParticleCoor *particleEntry = new ParticleCoor();
+  TBranch *thermBranch = thermTree->GetBranch("particle");
+  thermBranch->SetAddress(particleEntry);
+
+  //Get first particle in this event
+  int nBytesInEntry = thermTree->GetEntry(firstEntry);
+  assert(nBytesInEntry > 0);
+  UInt_t currentEventID = particleEntry->eventid;
+
+  // Determine which of proton, antiproton, pi+, pi- we need
+  vector<Int_t> daughterPIDs;
+  if((parent1PDG > 0) || (parent2PDG > 0)) { //want lambdas
+    daughterPIDs.push_back(kProt);
+    daughterPIDs.push_back(kPiMinus);
+  }
+  if((parent1PDG < 0) || (parent2PDG < 0)) { // want antilambdas
+    daughterPIDs.push_back(kAntiProt); 
+    daughterPIDs.push_back(kPiPlus); 
+  }
+  
+  //loop over all the particles in the event
+  while(particleEntry->eventid == currentEventID)
+  {
+
+
+    //check if particle matcheS one of daughterPIDs
+    //check if it has a lambda parent
+    //check if the daughter passes cuts
+    //add parent track ID to list
+
+  }
+
+  for(/* each parent trac ID*/)
+  {
+    //check if it appears twice in the list (once for each daughter)
+    //check if it passes cuts
+    //add track ID to final list
+  }
+
+  delete particleEntry;
+
+  return lambdaIDs;
+}
+
+vector<Int_t> GetParentIDs(vector<Int_t> &lambdaIDs, Int_t parentPDG, TTree *thermTree)
+{
+
+}
+
+void FillTransformMatrix(TH2D *hTransform, vector<Int_t> &parent1IDs, vector<Int_t> &parent2IDs, vector<Int_t> &lambdaIDs, TTree *thermTree);
 
