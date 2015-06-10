63134d03264877071df3fc349ceb7fcba7ab45a9
Jai Salzwedel <jai.salzwedel@cern.ch>
2015-06-10 16:13:18 -0400

    Filled out SetupMatrixHist()

--- TransformMatrices/GenerateTransformMatrix.C
+++ TransformMatrices/GenerateTransformMatrix.C
@@ -19,7 +19,7 @@ void GenerateTransformMatrix(const Int_t nFiles)
   Int_t parent1PDG; //Pass this in somehow
   Int_t parent2PDG;
 
-  TH2D *hTransform = SetupMatrixHist();
+  TH2D *hTransform = SetupMatrixHist(parent1PDG, parent2PDG);
   
   vector<TString> fileNames = GetTFileNames(nFiles);
 
@@ -51,16 +51,42 @@ void GenerateTransformMatrix(const Int_t nFiles)
 
 
 
-TH2D *SetupMatrixHist()
+TH2D *SetupMatrixHist(Int_t parent1PDG, Int_t parent2PDG)
 {
   // Make the transform matrix histogram
   // Setup the axis labels
   // Make the plot pretty
   // Take as inputs the pdg codes or axis labels 
+  
+  TString part1Name = GetNameFromPDG(parent1PDG);
+  TString part2Name = GetNameFromPDG(parent2PDG);
+  TString histName = "TransformMatrix";
+  histName += part1Name;
+  histName += part2Name;
+  Int_t kstarBins = 800;
+  Double_t maxKstar = 2.;
+  TH2D *hTransform = new TH2D(histName, histName, 
+			      kstarBins, 0., maxKstar,
+			      kstarBins, 0. maxKstar);
+  TString xLabel = "k^{*}_{#Lambda#Lambda}";
+  TString yLabel = "k^{*}_{#";
+  yLabel += part1Name;
+  yLabel += "#";
+  yLabel += part2Name;
+  yLabel += "}";
 
-  TH2D *hTransform;
-  // ...
+  hTransform->GetXaxis()->SetTitle(xLabel);
+  hTransform->GetYaxis()->SetTitle(yLabel);
+  hTransform->SetNdivisions(505,"XY");
   return hTransform;
+}
+
+TString GetNameFromPDG(Int_t pdgCode)
+{
+  //Take a pdg code and return a TString of the particle's name
+  TString pdgName;
+  //...
+  return pdgName;
 }
 
 vector<TString> GetTFileNames(const Int_t nFiles)
